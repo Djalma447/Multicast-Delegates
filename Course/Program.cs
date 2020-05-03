687d0a87da969c8debc87dab31cf7c1f93035a0c
Djalma <djalma.ant@gmail.com>
2020-05-03 02:33:46 -0300

    Multicast Delegates

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Course.Services;
 
 namespace Course
 {
     class Program
     {
+
+        delegate void BinaryNumericOperation(double n1, double n2); // Multicast Delegate - Um Delegate que pode servir de referência para mais de um Método, porém, seu uso faz sentido para Métodos 'void'
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double a = 12;
+            double b = 10;
+
+            BinaryNumericOperation op = CalculationService.ShowSum; // Declarando um objeto do tipo Delegate e usando-o como referência para um Método Static
+            op += CalculationService.ShowMax; // Acrescentando outro Método Static para o Delegate declarado
+            op(a, b); // Os Métodos são feitos na ordem que foram acrescentados
         }
     }
 }
